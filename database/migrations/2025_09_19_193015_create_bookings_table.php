80d18db84ef3651034db024fba6799966303c370
Robert Krampe <robert.krampe@gmx.net>
2025-09-19 21:37:08 +0200

    added CashBox and Booking model and migration

---
+++ database/migrations/2025_09_19_193015_create_bookings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('description', 255);
+            $table->decimal('amount', 10, 2);
+            $table->date('booking_date');
+            $table->foreignId('cash_box_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
